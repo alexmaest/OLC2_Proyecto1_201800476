bddd943751263819c72c09cf7cf1fa5d1e2b3140
alexmaest <80006030+alexmaest@users.noreply.github.com>
2022-08-23 21:23:43 -0600

    Delete ListArrayMultiple.py

--- AST/Instructions/ListArrayMultiple.py
+++
@@ -1,7 +0,0 @@
-from AST.Abstracts.Instruccion import Instruccion
-from AST.Abstracts.Retorno import Retorno, TYPE_DECLARATION
-
-class ListArrayMultiple(Instruccion):
-
-    def __init__(self, expression):
-        self.expression = expression
